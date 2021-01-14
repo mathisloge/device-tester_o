fb42f36076f9a14eceb659a9c6c2b2e3d66a1759
Mathis Logemann <mathisloge@gmail.com>
2021-01-14 22:34:41 +0100

    udp part 1

--- app/src/connection/udp_connection.cpp
+++ app/src/connection/udp_connection.cpp
@@ -1,1 +1,60 @@
-#include "udp_receiver_connection.hpp"
+#include "udp_connection.hpp"
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+
+UdpConnection::UdpConnection(ConnectionHandle &handle, const std::string &identifier, boost::asio::io_context &io_context)
+    : Connection(handle, identifier),
+      strand_{boost::asio::make_strand(io_context)},
+      socket_{strand_}
+{
+}
+bool UdpConnection::isConnected() const
+{
+  return should_receive_;
+}
+void UdpConnection::connect()
+{
+  should_receive_ = true;
+  startRead();
+}
+void UdpConnection::disconnect()
+{
+  should_receive_ = false;
+}
+
+void UdpConnection::setOption(const std::string &write_address, const unsigned short send_port)
+{
+}
+void UdpConnection::setOption(const unsigned short resv_port)
+{
+}
+
+void UdpConnection::startRead()
+{
+  socket_.async_receive_from(
+      boost::asio::buffer(buffer_rx_),
+      rx_endpoint_,
+      std::bind(&UdpConnection::handleRead, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+}
+void UdpConnection::startWrite()
+{
+}
+
+void UdpConnection::handleRead(const boost::system::error_code &error, std::size_t n)
+{
+  if (!error)
+  {
+    handle_.processData(std::span<uint8_t>(buffer_rx_.begin(), buffer_rx_.begin() + n));
+    buffer_rx_.fill(0);
+    if (should_receive_)
+      startRead();
+  }
+  else
+  {
+    SPDLOG_ERROR("Error while receiving {}", error.message());
+    disconnect();
+  }
+}
+void UdpConnection::handleWrite(const boost::system::error_code &error)
+{
+}
--- app/src/connection/udp_connection.hpp
+++ app/src/connection/udp_connection.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <array>
 #include <boost/asio.hpp>
 #include "connection.hpp"
 class UdpConnection : public std::enable_shared_from_this<UdpConnection>, public Connection
@@ -12,8 +13,20 @@ public:
     void connect() override;
     void disconnect() override;
 
-    void setOption(const std::string &target, const unsigned short send_port);
+    void setOption(const std::string &write_address, const unsigned short send_port);
     void setOption(const unsigned short resv_port);
 
 private:
+    void startRead();
+    void startWrite();
+    void handleRead(const boost::system::error_code &error, std::size_t n);
+    void handleWrite(const boost::system::error_code &error);
+
+private:
+    /// Strand to ensure the connection's handlers are not called concurrently.
+    boost::asio::strand<boost::asio::executor> strand_;
+    udp::socket socket_;
+    std::array<uint8_t, 1024> buffer_rx_;
+    udp::endpoint rx_endpoint_;
+    bool should_receive_;
 };
