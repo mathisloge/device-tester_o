e1eff89e944d7a32d97dd4305d09d9fb26c8979a
Mathis Logemann <mathisloge@gmail.com>
2021-02-24 19:49:27 +0100

    removed obsolete dataflow folder

--- data-flow/src/nodes/core/json.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <nlohmann/json.hpp>
-#include "data-flow/nodes/core/base_node.hpp"
-
-namespace dt::df
-{
-    //! these will directly be implemented in base_node.cpp
-    void to_json(nlohmann::json &j, const BaseNode &p);
-} // namespace dt::df
